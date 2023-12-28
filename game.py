b91e5119a7d214c16d1c71a6e6573d9cc60fe098
Debbie <tkudebby@gmail.com>
2023-12-28 18:56:26 +0800

    1.修改提示數字大小 2.修復bug 3.增加測試值z

--- game.py
+++ game.py
@@ -1,17 +1,23 @@
 import random
-#1~50
-#使用者猜5次
-#猜對提前離開
-#猜錯提示答案
 
-#題目
-x=random.randint(1,50)
+# 1~50
+# 使用者猜5次
+# 猜對提前離開
+# 猜錯提示答案
+
+# 題目
+x = random.randint(1, 50)
 print(x)
+# z=10
 
-for i in range(5):    
-    y=int(input('請猜數字(1~50):'))
-    if y==x:
-        print('猜對了')
+
+for i in range(5):
+    y = int(input("請猜數字(1~50):"))
+    if y == x:
+        print("猜對了")
         break
     else:
-        print('猜錯了')
+        if y > x:
+            print("猜低一點")
+        else:
+            print("猜高一點")
