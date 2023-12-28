a1a39eb32d62c0007c82f045e45a9ae2bc9cefbc
Debbie <tkudebby@gmail.com>
2023-12-28 20:33:52 +0800

    完成猜數字區間提示功能

--- game.py
+++ game.py
@@ -4,21 +4,34 @@ import random
 # 使用者猜5次
 # 猜對提前離開
 # 猜錯提示答案
+# 提示區間變動
 
 # 題目
-x = random.randint(1, 50)
-print(x)
+
+start = 1
+end = 50
+
+x = random.randint(start, end)
+print(f"範圍:{start}~{end}")
+# print(x)
 # z=10
 # 測試分支功能
 
-
 for i in range(5):
-    y = int(input("請猜數字(1~50):"))
+    y = int(input(f"請猜數字({start}~{end}):"))
     if y == x:
         print("猜對了")
         break
     else:
         if y > x:
+            if end > y:
+                end = y - 1
             print("猜低一點")
         else:
+            if start < y:
+                start = y + 1
             print("猜高一點")
+if y != x:
+    print(f"猜錯了，答案是{x}")
+else:
+    print(f"恭喜過關，共猜了{i+1}次")
